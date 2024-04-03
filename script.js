79ff4f821f303f1777a3d7c4b64553797891f574
Mariam Undilashvili <mariam.undilashvili1@gmail.com>
2024-04-03 18:59:06 +0400

    BRN
    BRN

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 
 yesBtn.addEventListener("click", () => {
-  question.innerHTML = "Yayyyyyyyyyyyyyyyyyyyyyyy";
+  question.innerHTML = "Great! I will be in Brno on 7th, from 7pm. See ya!";
   gif.src =
     "https://media.giphy.com/media/SjxtmBS267NOU/giphy.gif";
 });
